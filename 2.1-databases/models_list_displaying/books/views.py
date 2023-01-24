cbef24d630dad0365c65dd84cc8e5651d920ed35
Dan Kozlov <kozlov-da@iszf.irk.ru>
2023-01-24 10:40:25 +0800

    hw 2.1.2 book catalog

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
+from .converters import DateConverter
+
+from books.models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    context = {'books': Book.objects.all()}
+    return render(request, template, context)
+
+
+def books_on_date(request, pub_date):
+    template = 'books/books_on_date.html'
+    converter = DateConverter()
+    try:
+        current_date = converter.to_python(value=pub_date)
+        books = Book.objects.filter(pub_date=current_date)
+        next_books = Book.objects.filter(pub_date__gt=current_date).order_by('pub_date')
+        prev_books = Book.objects.filter(pub_date__lt=current_date).order_by('-pub_date')
+        next_date = next_books[0].pub_date if next_books else None
+        prev_date = prev_books[0].pub_date if prev_books else None
+    except ValueError:
+        books = None
+        current_date = None
+        prev_date = None
+        all_books = Book.objects.order_by('pub_date')
+        next_date = all_books[0].pub_date if all_books else None
+    context = {
+            'books': books,
+            'next_date': next_date,
+            'prev_date': prev_date,
+            'current_date': current_date,
+              }
     return render(request, template, context)
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from books.views import books_view
+from books import views
 
 urlpatterns = [
-    path('', books_view, name='books'),
     path('admin/', admin.site.urls),
+    path('books/', views.books_view, name='books'),
+    path('books/<pub_date>/', views.books_on_date),
 ]
