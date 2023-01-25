80309a129dd518379eef54c6c6a5d44ad803f24e
Dan Kozlov <kozlov-da@iszf.irk.ru>
2023-01-25 15:27:29 +0800

    Minor fix

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator
+from django.http import Http404
 from django.shortcuts import render
 from .converters import DateConverter
 
@@ -13,23 +15,27 @@ def books_view(request):
 def books_on_date(request, pub_date):
     template = 'books/books_on_date.html'
     converter = DateConverter()
+    books = None
+    current_date = None
+    next_date = None
+    prev_date = None
     try:
-        current_date = converter.to_python(value=pub_date)
-        books = Book.objects.filter(pub_date=current_date)
-        next_books = Book.objects.filter(pub_date__gt=current_date).order_by('pub_date')
-        prev_books = Book.objects.filter(pub_date__lt=current_date).order_by('-pub_date')
-        next_date = next_books[0].pub_date if next_books else None
-        prev_date = prev_books[0].pub_date if prev_books else None
-    except ValueError:
-        books = None
-        current_date = None
-        prev_date = None
         all_books = Book.objects.order_by('pub_date')
-        next_date = all_books[0].pub_date if all_books else None
+        current_date = converter.to_python(value=pub_date).date()
+        all_pub_dates = all_books.values_list('pub_date', flat=True).distinct()
+        next_pub_dates = all_pub_dates.filter(pub_date__gt=current_date)
+        prev_pub_dates = all_pub_dates.filter(pub_date__lt=current_date).order_by('-pub_date')
+        if next_pub_dates:
+            next_date = next_pub_dates[0]
+        if prev_pub_dates:
+            prev_date = prev_pub_dates[0]
+        books = all_books.filter(pub_date=current_date)
+    except ValueError:
+        current_date = None
     context = {
             'books': books,
+            'current_date': current_date,
             'next_date': next_date,
             'prev_date': prev_date,
-            'current_date': current_date,
               }
     return render(request, template, context)
