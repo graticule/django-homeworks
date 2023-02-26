8b9d614154303df7ea91846721ee29a3b875ad61
Daniil Kozlov <daniil.a.kozlov@gmail.com>
2023-02-26 11:43:00 +0800

    hw 3.2

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,20 +1,31 @@
 from rest_framework import serializers
+from .models import Product, Stock, StockProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
     # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    pass
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all(), required=True)
+    quantity = serializers.IntegerField(min_value=1, default=1)
+    price = serializers.DecimalField(max_digits=18, decimal_places=2)
+
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price',]
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions',]
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -26,7 +37,8 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for position in positions:
+            StockProduct.objects.create(product=position['product'], stock=stock, quantity=position['quantity'], price=position['price'])
         return stock
 
     def update(self, instance, validated_data):
@@ -39,5 +51,8 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for position in positions:
+            StockProduct.objects.update_or_create(product=position['product'], stock=stock, quantity=position['quantity'], price=position['price'])
+        return stock
 
         return stock
