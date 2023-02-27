0624810f99039a5e6d95fb32a687b0458e1d3f51
graticule <34333953+graticule@users.noreply.github.com>
2023-02-27 08:56:57 +0800

    Added optional task

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -18,5 +18,6 @@ class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
-    filter_backends = [DjangoFilterBackend, ]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['products',]
+    search_fields = ['products__title', 'products__description',]
